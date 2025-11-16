9eba37023ef7da7ac0104bcc3e6fe3f8820b4421
cognoscentiq-ux <cognoscentiq@gmail.com>
2025-11-16 11:21:46 +0300

    added changes to the inner text bg color #E9BC76

--- App.tsx
+++ App.tsx
@@ -188,7 +188,7 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header name={user.name} userType={user.type} onLogout={handleLogout} />
-      <main ref={chatContainerRef} className="flex-1 overflow-y-auto p-4 md:p-6">
+      <main ref={chatContainerRef} className="flex-1 overflow-y-auto p-4 md:p-6 bg-brand-accent">
         <div className="max-w-4xl mx-auto">
           {messages.map((msg) => (
             <ChatMessage key={msg.id} message={msg} sources={sourcesByMsgId[msg.id]} />
